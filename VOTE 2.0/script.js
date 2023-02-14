882ed41a68f355fcba8e3c92ee54625282db5a88
Pawan Kamat <114341013+keepupwithpawan@users.noreply.github.com>
2023-02-14 13:20:02 +0530

    Add files via upload

---
+++ VOTE 2.0/script.js
@@ -0,0 +1,21 @@
+function home() {
+    window.location.href="http://127.0.0.1:5500/index.html";
+}
+
+function about() {
+  document.getElementById("boxes").scrollIntoView({behavior: "smooth"});
+}
+
+function aboutimg() {
+  window.location.href="http://127.0.0.1:5500/about.html";
+}
+
+function trans() {
+    document.getElementById("contactcontents").scrollIntoView({behavior: "smooth"});
+}
+
+function reg() {
+  window.location.href="http://127.0.0.1:5500/register.html";
+}
+
+
